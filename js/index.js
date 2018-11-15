5955afc37386b3b69b032c160255989b9dcd37ce
weijun huang <luck938938@hotmail.com>
2018-11-15 02:24:11 +0000

    automatically backed up by learn

--- js/index.js
+++ js/index.js
@@ -7,3 +7,13 @@ element.style.backgroundColor = '#f9f9f9';
 document.body.appendChild(element);
 
 element.style.textAlign = 'center';
+
+var ul = document.createElement('ul');
+ 
+for (let i = 0; i < 3; i++) {
+  let li = document.createElement('li');
+  li.innerHTML = (i + 1).toString();
+  ul.appendChild(li);
+}
+ 
+element.appendChild(ul);
